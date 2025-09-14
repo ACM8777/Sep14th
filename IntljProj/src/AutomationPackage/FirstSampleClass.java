ad59bdb1c0e4d6c9d7d664a83f621c5cfb593e85
ACMTesting <acmpmgv@gmail.com>
2025-09-14 20:50:25 +0530

    Initial project draft

---
+++ IntljProj/src/AutomationPackage/FirstSampleClass.java
@@ -0,0 +1,9 @@
+package AutomationPackage;
+
+public class FirstSampleClass {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("I am super");
+    }
+}
